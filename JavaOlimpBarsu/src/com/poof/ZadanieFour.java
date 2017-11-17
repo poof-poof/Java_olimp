917ed7ef874ddbce053b861f81a879a1fe400586
poof-poof <lexa10.95@mail.ru>
2017-11-17 23:34:15 +0300

    edit class ZadanieFour

--- JavaOlimpBarsu/src/com/poof/ZadanieFour.java
+++ JavaOlimpBarsu/src/com/poof/ZadanieFour.java
@@ -1,6 +1,5 @@
 package com.poof;
 
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
 import java.util.Vector;
@@ -15,21 +14,25 @@ public class ZadanieFour {
         int countRows = 0;
         int availableSeats = COUNT_PLACES;
         Vector<Integer> countPeople = new Vector<>();
+
         do {
             System.out.print("Введите количество групп (1 <= n <= 100) : ");
             groups = sc.nextInt();
         }while (groups < 1 || groups > 100);
+
         System.out.println("Введите количество сотрудников для каждой из групп (1 <= a <= 6) :");
+
         for (int i = 0; i < groups; i++){
             countPeople.add(sc.nextInt());
         }
+
         countPeople.sort(new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
                 return o1 - o2;
             }
         });
-        int iter = 0;
+
         do {
             if (availableSeats == 0 || availableSeats < countPeople.get(0)){
                 countRows++;
@@ -38,11 +41,10 @@ public class ZadanieFour {
             }else if(availableSeats >= countPeople.get(0)){
                 availableSeats -= countPeople.get(0);
                 countPeople.remove(0);
-                iter++;
                 continue;
             }
         }while (!countPeople.isEmpty());
+
         System.out.println("Минимальное число рядов из 6-ти кресел : " + countRows);
     }
-
 }
