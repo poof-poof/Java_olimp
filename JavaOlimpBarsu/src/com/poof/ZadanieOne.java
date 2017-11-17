905f6c6b07888ef518954d54ef33064563d0acbb
poof-poof <lexa10.95@mail.ru>
2017-11-17 11:46:30 +0300

    finish class ZadanieOne

--- JavaOlimpBarsu/src/com/poof/ZadanieOne.java
+++ JavaOlimpBarsu/src/com/poof/ZadanieOne.java
@@ -1,4 +1,27 @@
 package com.poof;
 
+import java.util.Scanner;
+
 public class ZadanieOne {
+    public static final int BORDER = 13;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        do {
+            System.out.print("Введите значение N > " + BORDER +" : ");
+            n = sc.nextInt();
+        }while(n <= BORDER);
+        for (int i = 0; i < n; i++){
+            if (primeNumber(i) && primeNumber(i + 2))
+                System.out.println("Близнецы : " + i + " и " + (i + 2) + ";");
+        }
+    }
+
+    static boolean primeNumber(int number){
+        for (int i = 2; i < number; i++){
+            if (number % i == 0)
+                return false;
+        }
+        return true;
+    }
 }
